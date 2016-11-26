0679e385a24a2c9a48d48adcc18168ab844bc635
David <youchen.du@gmail.com>
2016-11-26 10:38:14 +0800

    [add] abstract login logic.

--- home/views.py
+++ home/views.py
@@ -3,15 +3,31 @@
 # Author: David
 # Email: youchen.du@gmail.com
 # Created: 2016-11-26 09:42
-# Last modified: 2016-11-26 10:11
+# Last modified: 2016-11-26 10:30
 # Filename: views.py
 # Description:
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 home = Blueprint('home', 'home',
                  template_folder='templates')
 
 
-@home.route('/login')
+@home.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('home/login.html')
+    if request.method == 'GET':
+        return render_template('home/login.html')
+    elif request.method == 'POST':
+        if validate_user():
+            return redirect(url_for('school.table'))
+        else:
+            context = {}
+            error = u'请检查您的用户名和密码是否正确。'
+            context['error'] = error
+            return render_template('home/login.html', **context)
+
+
+def validate_user():
+    if request.form['username'] == 'david' and request.form['password'] == '1':
+        return True
+    else:
+        return False
