807dda61325a3dc0169ce3a4f21b2b98b99a0ced
David <youchen.du@gmail.com>
2016-11-27 10:02:07 +0800

    [update] PEP-8 style.

--- const/models.py
+++ const/models.py
@@ -1,27 +1,37 @@
-#!/usr/bin/env python
-# coding=utf-8
-
+#!/usr/bin/python
+# coding: UTF-8
+# Author: David
+# Email: youchen.du@gmail.com
+# Created: 2016-11-27 09:58
+# Last modified: 2016-11-27 10:02
+# Filename: models.py
+# Description:
 from __future__ import unicode_literals
 from django.db import models
 
+
 class Province(models.Model):
     name = models.CharField(max_length=50)
-    
+
     class Meta:
         verbose_name = u"省份"
         verbose_name_plural = u"省份"
+
     def __unicode__(self):
         return "%s" % self.name
+
 
 class School(models.Model):
     province = models.ForeignField(Province)
     name = models.CharField(max_length=100)
-    
+
     class Meta:
         verbose_name = u"院校"
         verbose_name_plural = u"院校"
+
     def __unicode__(self):
         return "%s" % self.name
+
 
 class QuestionItem(models.Model):
     text = models.CharField(max_length=200)
@@ -30,30 +40,35 @@ class QuestionItem(models.Model):
     class Meta:
         verbose_name = u"问题项"
         verbose_name_plural = u"问题项"
+
     def __unicode__(self):
         return "%s" % self.text
+
 
 class AnswerSheet(models.Model):
     school = models.ForeignField(School)
     year = models.IntegerField()
-    finished = models.BooleanField(default=False) #是否确认提交
-    complete_cnt = models.IntegerField(default=0) #未完成的项数
+    finished = models.BooleanField(default=False)  # 是否确认提交
+    complete_cnt = models.IntegerField(default=0)  # 未完成的项数
 
     class Meta:
         verbose_name = u"问卷"
         verbose_name_plural = u"问卷"
+
     def __unicode__(self):
         return "%s %d" % (self.school.name, self.year)
+
 
 class AnswerItem(models.Model):
     question_item = models.ForeignField(QuestionItem)
     answer_sheet = models.ForeignField(AnswerSheet)
     number = models.IntegerField(default=0)
     path = models.CharField(max_length=200, blank=True, null=True)
-    complete = models.BooleanField(default=False) #是否完成过
+    complete = models.BooleanField(default=False)  # 是否完成过
 
     class Meta:
         verbose_name = u"回答项"
         verbose_name_plural = u"回答项"
+
     def __unicode__(self):
         return "%s:%s" % (self.question_item.text, self.number)
