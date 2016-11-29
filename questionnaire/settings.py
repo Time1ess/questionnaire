9a7ad4052af6e4921c0d21728844aa6b4369f5b4
obelisk-the-mxl <mxl.obelisk@hotmail.com>
2016-11-29 09:15:06 +0800

    mysql settings

--- questionnaire/settings.py
+++ questionnaire/settings.py
@@ -89,9 +89,17 @@ WSGI_APPLICATION = 'questionnaire.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'questiontest',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '192.168.1.124',
+        'PORT': 3306
     }
 }
 
