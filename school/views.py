fc0ccbeed480e1e3bd1ec73576b76c6f632e18ed
obelisk-the-mxl <mxl.obelisk@hotmail.com>
2016-12-01 09:40:59 +0800

    school table view

--- school/views.py
+++ school/views.py
@@ -10,19 +10,23 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from const import SCHOOL_SHEET
-from const.models import QuestionSheet,School,QuestionItem, AnswerSheet, AnswerItem
+from const.models import QuestionSheet, AnswerSheet, AnswerItem
 
 @login_required
 def question_table_view(request):
     q_sheet = QuestionSheet.objects.filter(category=SCHOOL_SHEET).last()
-    school = request.user.school
-    q_items = QuestionItem.objects.filter(question_sheet=q_sheet).order_by('index')
-    
-    a_sheet = AnswerSheet.objects.filter(user=request.user).filter(question_sheet=q_sheet).last()
-    print dir(a_sheet)
-    
+    a_sheet, _ = AnswerSheet.objects.get_or_create(user=request.user,
+                                                question_sheet=q_sheet)
+
+
+    q_items = q_sheet.questionitem_set.all()
+    for q_item in q_items:
+        AnswerItem.objects.get_or_create(question_item=q_item,
+                                        answer_sheet=a_sheet)
+
+    a_items = a_sheet.answeritem_set.all().select_related('question_item').order_by('question_item__index')
 
     context = {
-        "q_items": q_items
+        "a_items": a_items
     }
     return render(request, 'school/question_table.html', context)
